14736685861f9f79d40092fd668b00ed77e4c5ef
Domenico Cosentino <d.cosentino@4sigma.it>
2015-06-07 20:24:26 +0200

    Added a decorator that can be used to redirect the visitor to the cookie policy page when it has not been accepted

--- cookie_policy/urls.py
+++ cookie_policy/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^policy/?', views.policy_view, name='policy'),
+    url(r'^policy/?', views.policy_view, name='cookiePolicy'),
     url(r'^accept-cookie-policy/?', views.acceptCookiePolicy_view, name='acceptCookiePolicy'),
 ]
